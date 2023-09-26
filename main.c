be0d7642965e65635f49eb872d23d44efd41caa1
Josué Medino <josuemedino5@gmail.com>
2023-09-25 22:11:36 -0300

    refact: utilizacao de parâmetros string em gerarChave()

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <gmp.h>
 #include <stdlib.h>
 
-int initialChoose(){
+int escolhaInicial(){
     
     printf("1-Gerar chave pública\n");
     printf("2-Encriptar\n");
@@ -14,29 +14,29 @@ int initialChoose(){
     if(escolha>3 || escolha < 1){
         system("clear");
         printf("*Digite uma escolha válida*\n\n");
-        return initialChoose();
+        return escolhaInicial();
     }
     system("clear");
     return escolha;
 }
 
-void chavePublica(){
-    //Declara e inicializa as variáveis 
-    mpz_t p, q, e, n; 
-    mpz_inits(p, q, e, n, NULL);
+void chavePublica(char p_string[100],char q_string[100], char e_string[100]){
 
-    //Recebe os valores  
-    printf("Digite os valores p, q, e, respectivamente.\n");
-    gmp_scanf("%Zd %Zd %Zd", p, q, e);    
+    //Declarando as variáveis mpz
+    mpz_t p,q,e,n;
+    mpz_inits(p, q,e,n, NULL);
 
-    //### COLOCAR UMA FUNÇÃO QUE VERIFICA SE  P, Q É COMPRIMO A (P-1)(Q-1) ###
+    //Convertendo a entrada para mpz (Inteiro Arbitrariamente Grande)
+    mpz_init_set_str(p, p_string, 10);
+    mpz_init_set_str(q, q_string, 10);
+    mpz_init_set_str(e, e_string, 10);
 
     //Calcula a Chave Publica 
     mpz_mul(n, p, q);
 
-    gmp_printf("p: %Zd | q: %Zd | e: %Zd | n: %Zd\n", p, q, e, n);
-
-    //Converte os dados em String
+    //gmp_printf("p: %Zd | q: %Zd | e: %Zd | n: %Zd\n", p, q, e, n);
+    printf("TXT ALTERADO!\n");
+    //Converte os dados em String//EXCLUIR DPS DESNECESSÁRIO
     char *str_q, *str_p, *str_n, *str_e;
     str_p =  mpz_get_str(NULL, 10, p);
     str_q =  mpz_get_str(NULL, 10, q);
@@ -50,40 +50,55 @@ void chavePublica(){
     fclose(file);
     
     //Limpa memória alocada
-    mpz_clears(p, q, e, n, NULL);
+    mpz_clears(p, q, e,n, NULL);
     free(str_p); free(str_q); free(str_e); free(str_n);
 
     return;
 }
 
-void encripitar(){
-    
+void encriptar(){
+        printf("Entrou no  encriptar()\n");
 
+    
     return;
 }
+
+void  desencriptar(){
+    printf("Entrou no  desencriptar()\n");
+    
+    return;
+}
+
+
 int main(){
     int voltar;
     do{
-    system("clear");
-    int escolha = initialChoose();
-    
-    switch(escolha) {
-    case 1:
-        chavePublica();
+        system("clear");
+        int escolha = escolhaInicial();
+        
+        switch(escolha) {
+        case 1:
+            //Lendo a entrada como string
+            char p_string[100], q_string[100], e_string[100];
+            printf("Digite os valores de p q e:\n");
+            scanf(" %s %s %s", p_string, q_string, e_string);
+
+            chavePublica(p_string, q_string,e_string);
+            break;
+        case 2:
+            encriptar();
+            
+
+            break;
+        case 3:
+            desencriptar();
+            break;
+        }
+
         printf("Voltar a tela inicial? [1,0] \n");
         scanf("%d", &voltar);
-        break;
-    case 2:
-        encripitar();
-        break;
-    case 3:
-        //desencriptar();
-        break;
-    }
 
     } while (voltar);
-    
-    
     
     return 0;
 }
